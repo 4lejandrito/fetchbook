e802a51be344311b892d278d9e009ff3903dc102
Alejandro Tardín <alejandro@tardin.com>
2023-10-13 18:18:06 +0200

    refactor: inlining

--- test/server.ts
+++ test/server.ts
@@ -7,17 +7,17 @@ export default {
   start: async () =>
     new Promise<void>((resolve) => {
       const app = jsonServer.create();
-      const router = jsonServer.router({
-        posts: [
-          {
-            id: 1,
-            it: "works!",
-          },
-        ],
-      });
-      const middlewares = jsonServer.defaults();
-      app.use(middlewares);
-      app.use(router);
+      app.use(jsonServer.defaults());
+      app.use(
+        jsonServer.router({
+          posts: [
+            {
+              id: 1,
+              it: "works!",
+            },
+          ],
+        }),
+      );
       server = app.listen(3000, resolve);
     }),
   stop: () => server?.close(),
